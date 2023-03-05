065867bf73ef752361b3e09865a909727d3f1fd1
burzhun <b.burzhunov@ficto.ru>
2023-03-05 12:36:52 +0300

    fix routing

--- src/index.js
+++ src/index.js
@@ -1,15 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-//const basename = window.location
-console.log(window.location);
 root.render(
   <React.StrictMode>
-    <BrowserRouter basename="/">
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 );
